f2f435aeac0d7abb8e9fbdb8961369c5abf3fe84
JOSHNA-25 <joshnass250306@gmail.com>
2025-06-26 12:35:52 +0530

    Initial commit with multiple PHP files

---
+++ ProjectAsscr.php
@@ -0,0 +1,35 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $product = $_GET['product'];
+    $name = $_GET['name'];
+    $address = $_GET['address'];
+    $phone = $_GET['phone'];
+    $pincode = $_GET['pincode'];
+    $paymentmode = $_GET['paymentmode'];
+
+    $servername = "localhost";
+    $username = "root";
+    $db_password = "";
+    $dbname = "login";
+
+    $conn = new mysqli($servername, $username, $db_password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO accessories (product,name, address, phone, pincode, paymentmode)
+            VALUES ('$product','$name', '$address', '$phone', '$pincode', '$paymentmode')";
+
+   if ($conn->query($sql) === TRUE) {
+    echo "<script>
+        alert('✅ Payment Successful!');
+        window.location.href = 'Project1.html';
+    </script>";
+    exit();
+}
+
+
+    $conn->close();
+}
+?>
---
+++ ProjectFotwr.php
@@ -0,0 +1,36 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $product = $_GET['product'];
+    $size = $_GET['size'];
+    $name = $_GET['name'];
+    $address = $_GET['address'];
+    $phone = $_GET['phone'];
+    $pincode = $_GET['pincode'];
+    $paymentmode = $_GET['paymentmode'];
+
+    $servername = "localhost";
+    $username = "root";
+    $db_password = "";
+    $dbname = "login";
+
+    $conn = new mysqli($servername, $username, $db_password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO footwear (product, size, name, address, phone, pincode, paymentmode)
+            VALUES ('$product','$size', '$name', '$address', '$phone', '$pincode', '$paymentmode')";
+
+   if ($conn->query($sql) === TRUE) {
+    echo "<script>
+        alert('✅ Payment Successful!');
+        window.location.href = 'Project1.html';
+    </script>";
+    exit();
+}
+
+
+    $conn->close();
+}
+?>
