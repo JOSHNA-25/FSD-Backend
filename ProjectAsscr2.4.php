cb722de8f847502e789a763c862b96ae4d4c9756
JOSHNA S <joshnas250306@gmail.com>
2025-07-09 00:14:24 +0530

    ProjectAsscr2.4.php

---
+++ ProjectAsscr2.4.php
@@ -0,0 +1,35 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $product = $_GET['product'];
+    $name = $_GET['name'];
+    $address = $_GET['address'];
+    $phone = $_GET['phone'];
+    $pincode = $_GET['pincode'];
+    $paymentmode = $_GET['paymentmode'];
+
+    $servername = "localhost";
+    $username = "root";
+    $db_password = "";
+    $dbname = "ecommerce";
+
+    $conn = new mysqli($servername, $username, $db_password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO watches_handbags_4 (product, name, address, phone, pincode, paymentmode)
+            VALUES ('$product', '$name', '$address', '$phone', '$pincode', '$paymentmode')";
+
+   if ($conn->query($sql) === TRUE) {
+    echo "<script>
+        alert('Thank you for Shopping!');
+        window.location.href = 'Project1.html';
+    </script>";
+    exit();
+}
+
+
+    $conn->close();
+}
+?>
