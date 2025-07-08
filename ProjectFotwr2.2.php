6c85abf5a358797763f1c7159b2d70e5d60c384b
JOSHNA S <joshnas250306@gmail.com>
2025-07-09 00:18:06 +0530

    ProjectFotwr2.2.php

---
+++ ProjectFotwr2.2.php
@@ -0,0 +1,36 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $color = $_GET['color'];
+    $size = $_GET['size'];
+    $name = $_GET['name'];
+    $address = $_GET['address'];
+    $phone = $_GET['phone'];
+    $pincode = $_GET['pincode'];
+    $paymentmode = $_GET['paymentmode'];
+
+    $servername = "localhost";
+    $username = "root";
+    $db_password = "";
+    $dbname = "ecommerce";
+
+    $conn = new mysqli($servername, $username, $db_password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO sneakers_2 (color, size, name, address, phone, pincode, paymentmode)
+            VALUES ('$color','$size', '$name', '$address', '$phone', '$pincode', '$paymentmode')";
+
+   if ($conn->query($sql) === TRUE) {
+    echo "<script>
+        alert('Thank you for Shopping!');
+        window.location.href = 'Project1.html';
+    </script>";
+    exit();
+}
+
+
+    $conn->close();
+}
+?>
